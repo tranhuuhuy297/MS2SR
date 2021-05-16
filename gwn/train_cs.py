8143c62e27d2b4723341b77af340306afc884ecc
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2021-05-16 16:48:43 +0700

    add test cs without gwn

--- gwn/train_cs.py
+++ gwn/train_cs.py
@@ -52,13 +52,28 @@ def get_phi(args, top_k_index):
 
 
 def main(args, **model_kwargs):
-    # # psi
-    # psi = get_psi(args).matrix
+    device = torch.device(args.device)
+    args.device = device
+    if 'abilene' in args.dataset:
+        args.nNodes = 12
+        args.day_size = 288
+    elif 'geant' in args.dataset:
+        args.nNodes = 22
+        args.day_size = 96
+    elif 'brain' in args.dataset:
+        args.nNodes = 9
+        args.day_size = 1440
+    elif 'sinet' in args.dataset:
+        args.nNodes = 74
+        args.day_size = 288
+    else:
+        raise ValueError('Dataset not found!')
 
-    # # phi
     train_loader, val_loader, test_loader, top_k_index = utils.get_dataloader(args)
+
     args.train_size, args.nSeries = train_loader.dataset.X_scaled_top_k.shape
-    # phi = get_phi(args, top_k_index)
+    args.val_size = val_loader.dataset.X_scaled_top_k.shape[0]
+    args.test_size = test_loader.dataset.X_scaled_top_k.shape[0]
 
     in_dim = 1
     if args.tod:
@@ -70,11 +85,70 @@ def main(args, **model_kwargs):
 
     args.in_dim = in_dim
 
-    aptinit, supports = utils.make_graph_inputs(args, args.device)
+    aptinit, supports = utils.make_graph_inputs(args, device)
+
     model = models.GWNet.from_args(args, supports, aptinit, **model_kwargs)
-    model.to(args.device)
+    model.to(device)
     logger = utils.Logger(args)
+
     engine = utils.Trainer.from_args(model, train_loader.dataset.scaler, train_loader.dataset.scaler_top_k, args)
+
+    utils.print_args(args)
+
+    if not args.test:
+        iterator = trange(args.epochs)
+
+        try:
+            if os.path.isfile(logger.best_model_save_path):
+                print('Model checkpoint exist!')
+                print('Load model checkpoint? (y/n)')
+                _in = input()
+                if _in == 'y' or _in == 'yes':
+                    print('Loading model...')
+                    engine.model.load_state_dict(torch.load(logger.best_model_save_path))
+                else:
+                    print('Training new model')
+
+            for epoch in iterator:
+                train_loss, train_rse, train_mae, train_mse, train_mape, train_rmse = [], [], [], [], [], []
+                for iter, batch in enumerate(train_loader):
+
+                    # x = batch['x']  # [b, seq_x, n, f]
+                    # y = batch['y']  # [b, seq_y, n]
+                    # sys.exit()
+                    x = batch['x_top_k']
+                    y = batch['y_top_k']
+
+                    if y.max() == 0: continue
+                    loss, rse, mae, mse, mape, rmse = engine.train(x, y)
+                    train_loss.append(loss)
+                    train_rse.append(rse)
+                    train_mae.append(mae)
+                    train_mse.append(mse)
+                    train_mape.append(mape)
+                    train_rmse.append(rmse)
+
+                engine.scheduler.step()
+                with torch.no_grad():
+                    val_loss, val_rse, val_mae, val_mse, val_mape, val_rmse = engine.eval(val_loader)
+                m = dict(train_loss=np.mean(train_loss), train_rse=np.mean(train_rse),
+                         train_mae=np.mean(train_mae), train_mse=np.mean(train_mse),
+                         train_mape=np.mean(train_mape), train_rmse=np.mean(train_rmse),
+                         val_loss=np.mean(val_loss), val_rse=np.mean(val_rse),
+                         val_mae=np.mean(val_mae), val_mse=np.mean(val_mse),
+                         val_mape=np.mean(val_mape), val_rmse=np.mean(val_rmse))
+
+                description = logger.summary(m, engine.model)
+
+                if logger.stop:
+                    break
+
+                description = 'Epoch: {} '.format(epoch) + description
+                iterator.set_description(description)
+        except KeyboardInterrupt:
+            pass
+
+    # Metrics on test data
     engine.model.load_state_dict(torch.load(logger.best_model_save_path))
     with torch.no_grad():
         test_met_df, x_gt, y_gt, y_real, yhat = engine.test(test_loader, engine.model, args.out_seq_len)
