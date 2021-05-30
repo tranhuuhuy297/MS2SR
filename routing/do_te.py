db5f062d6e4f5221835967e51fbf7e982ef3418f
anle <vananle1993@gmail.com>
2021-05-30 12:49:27 +0900

    no cs -> filling zeros

--- routing/do_te.py
+++ routing/do_te.py
@@ -60,7 +60,7 @@ def save_results(log_dir, fname, mlus, route_change):
 
 
 def prepare_te_data(x_gt, y_gt, yhat, args):
-    te_step = args.test_size if args.te_step is 0 else args.te_step
+    te_step = args.test_size if args.te_step == 0 else args.te_step
     x_gt = x_gt[0:te_step:args.seq_len_y]
     y_gt = y_gt[0:te_step:args.seq_len_y]
     if args.run_te == 'ls2sr' or args.run_te == 'onestep':
