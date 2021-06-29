8de2a016533700ddfd2989098a7b902b4884c2c9
anle <vananle1993@gmail.com>
2021-06-29 09:49:05 +0900

    fĩ gwx cs csp

--- gwn/train.py
+++ gwn/train.py
@@ -219,6 +219,9 @@ def main(args, **model_kwargs):
     if args.run_te != 'None':
         run_te(x_gt, y_gt, y_cs, args)
 
+    print('{} testset: {} x: {} y: {} topk:{} cs: {}'.format(args.dataset, args.testset, args.seq_len_x, args.seq_len_y,
+                                                             args.random_rate, args.cs))
+
 
 if __name__ == "__main__":
     args = utils.get_args()
