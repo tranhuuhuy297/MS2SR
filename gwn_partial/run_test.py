00b9db955014935bf28acd846b1b421b32af4bd4
vananle <vananle1993@gmail.com>
2021-07-02 12:42:56 +0900

    fix dataloader gwn cs

--- gwn_partial/run_test.py
+++ gwn_partial/run_test.py
@@ -39,6 +39,7 @@ def main():
 
                 cmd = 'python train.py --do_graph_conv --aptonly --addaptadj --randomadj'
                 cmd += ' --train_batch_size 64 --val_batch_size 64 --test'
+                # cmd += ' --train_batch_size 64 --val_batch_size 64 --test --run_te gwn_ls2sr'
                 cmd += ' --dataset {} --random_rate {} --testset {} --cs {} --device {}'.format(dataset_name,
                                                                                                 random_rate[d],
                                                                                                 test, cs, device)
