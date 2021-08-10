1dd2c71d4ef92a6cff1c144beb7e62feceed58e3
tranhuuhuy297 <46334604+tranhuuhuy297@users.noreply.github.com>
2021-08-10 16:41:25 +0700

    Update pursuit.py

--- pursuit.py
+++ pursuit.py
@@ -72,7 +72,7 @@ class MatchingPursuit(Pursuit):
                 if np.isclose(alpha, 0):
                     break
                 coeffs[gamma] += alpha
-                # coeffs[coeffs < 0] = 0
+                coeffs[coeffs < 0] = 0
                 i += 1
                 if self.sparsity:
                     finished = np.count_nonzero(coeffs) >= self.sparsity
@@ -166,7 +166,7 @@ class Solver_l0(Solver):
                 if np.isclose(alpha, 0):
                     break
                 coeffs[gamma] += alpha
-                # coeffs[coeffs < 0] = 0
+                coeffs[coeffs < 0] = 0
                 i += 1
                 if self.sparsity:
                     finished = np.count_nonzero(coeffs) >= self.sparsity
