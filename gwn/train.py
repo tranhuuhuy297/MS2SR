8fad26f79fade2a9c59b929251d56256663e17b0
anle <vananle1993@gmail.com>
2021-08-10 16:17:53 +0900

    debug

--- gwn/train.py
+++ gwn/train.py
@@ -206,8 +206,8 @@ def main(args, **model_kwargs):
             sparse = Solver_l0(A, max_iter=100, sparsity=int(args.mon_rate / 100 * y_cs.shape[-1])).fit(yhat[i].T)
 
             check_positive = sparse >= 0
-            print(check_positive)
-            if not (check_positive.all() is True):
+            check_positive = check_positive.astype(np.int)
+            if not (np.sum(check_positive) == sparse.size()):
                 raise RuntimeError
 
             y_cs[i] = np.dot(psi.matrix, sparse).T
